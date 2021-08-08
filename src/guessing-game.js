6c98a5c3d51700d1d03b5f65a96a7c7b4b26043a
VitaliSantalau <v.santalau@gmail.com>
2021-08-08 12:41:27 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+  
+  setRange(min, max) {  
+    this.arr = [];
+    for(let i = min; i <= max; i++) {
+      this.arr.push(i);
     }
+  }
 
-    guess() {
+  guess() {
+    this.middle = Math.floor(this.arr.length / 2);
+    return this.arr[this.middle];
+  }
 
-    }
+  lower() {
+    const min = this.arr[0];
+    const max = this.arr[this.middle];
+    this.setRange(min, max);
+  }
 
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    const min = this.arr[this.middle];
+    const max = this.arr[this.arr.length-1]
+    this.setRange(min, max);
+  }
 }
 
 module.exports = GuessingGame;
